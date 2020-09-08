00e226e3f9117339fe5e331e8bcf4674129d228a
Erne <ernesto92.mg@gmail.com>
2020-09-08 18:13:21 -0400

    refactor change title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default {
   */
   head: {
     titleTemplate: '%s',
-    title: 'Frontend Mentor | IP Address Tracker',
+    title: 'IP Address Tracker | Frontend Mentor',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
